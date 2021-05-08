fa5c42a2d20c9fbbed507bc12c9727e6375f566b
mRatna001 <77207977+mRatna001@users.noreply.github.com>
2021-05-08 17:09:37 +0100

    Add files via upload

---
+++ 07 05 2021 Mean. Median,Mode.py
@@ -0,0 +1,36 @@
+
+import csv
+with open("SOCR-HeightWeight.csv", newline="") as f:
+          reader= csv.reader(f)
+          file_data = list(reader)
+file_data.pop(0)
+new_data=[]
+for i in range(len(file_data)):
+    n_num = file_data[i][1]
+    new_data.append(float(n_num))
+n = len(new_data)
+total = 0
+for x in new_data:
+    total +=x
+
+mean= total/n
+          
+print("mean is: " + str(mean))
+new_data = []
+
+for i in range(len(file_data)):
+    n_num = file_data[i][1]
+    new_data.append(n_num)
+
+n = len(new_data)
+new_data.sort()
+
+if n% 2==  0:
+    median1 = float(new_data[n//2])
+    median2 = float(new_data[n//2-1])
+    median = (median1 + median2)/2
+else:
+    median = new_data[n//2]
+    print(n)
+print("median " + str(median))
+
